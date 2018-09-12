2dc6df3133bfda8c649de0ef7ea9efbff6cab2b6
daviddxu <daviddxu@cmail.carleton.ca>
2018-09-12 14:04:13 -0400

    Add existing file

--- src/test/java/assignment1/GameTest.java
+++ src/test/java/assignment1/GameTest.java
@@ -27,25 +27,34 @@ public class GameTest {
 	@Test
 	public void bustTest() {	//check if player has busted: exceeded 21 points
 		
-		
+		/**NOT IMPLEMENTED**/
+
 	}
 	@Test
 	public void hitTest() {	//test player's hit: deal the player another card and check if player busts
 		
-		
+		/**NOT IMPLEMENTED**/
 	}
 	
 	@Test
 	public void standTest() {	//test player's stand: mark all cards on table face up 
-		
+		/**NOT IMPLEMENTED**/
+
 		
 	}
 	
 	@Test
-	public void dealerPlayTest() {
-		
+	public void dealerPlayTest() {	//test dealer's play: if <= 16 points or a "soft 17", dealer hits otherwise stands
+		/**NOT IMPLEMENTED**/
+
 	}
 	
+	@Test
+	public void blackjackTest() {	//test blackjack checker function: if a player has an ace and 10, J, Q, K, they win; check dealer first then player; 
+		
+		/**NOT IMPLEMENTED**/
+
+	}
 	
 
 }
