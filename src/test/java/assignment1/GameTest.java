98b3167f440e5ca25f75607c399c9000e07f4c57
daviddxu <daviddxu@cmail.carleton.ca>
2018-09-12 13:51:24 -0400

    Add existing file

---
+++ src/test/java/assignment1/GameTest.java
@@ -0,0 +1,51 @@
+package assignment1;
+
+/*
+ * GameTest.java 
+ * TDD tester for Game.java (BlackJack) class
+ * 
+ */
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void dealTest() { //test dealer deal function: deal two cards to player and two to dealer
+		
+		//not implemented
+		fail("Not yet implemented");
+
+	}
+	
+	@Test
+	public void bustTest() {	//check if player has busted: exceeded 21 points
+		
+		
+	}
+	@Test
+	public void hitTest() {	//test player's hit: deal the player another card and check if player busts
+		
+		
+	}
+	
+	@Test
+	public void standTest() {	//test player's stand: mark all cards on table face up 
+		
+		
+	}
+	
+	@Test
+	public void dealerPlayTest() {
+		
+	}
+	
+	
+
+}
